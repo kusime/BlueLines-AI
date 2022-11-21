5ae3444e9d09b49a4b31fd750e73a9ebd5ec2de7
Kusime <mingcloudserver@gmail.com>
2022-11-21 22:56:25 +0800

    Ver 0.3.1 fix the BattleDoing Network Error confirm

--- STAT/Battle_Doing.py
+++ STAT/Battle_Doing.py
@@ -13,5 +13,14 @@ class BattleDoing(BaseState):
             # switch to the BattleDone State
             game.SwitchState(game.BattleDone)
             return
+        
+        # check if have confirm button , this cause by the net work connection error
+        # check if still have some comfirm information
+        predict = game.GenerticAI.getStablePredictions()
+        if (predict != None and predict[0]['label'] == game.GenerticButton.Confirm):
+            print("Confirming the Confirm ...")
+            game.Phone.tap(predict[0]['point'])
+            game.waiter()
+        
         print("waiting for game to finish...")
         game.waiter()
